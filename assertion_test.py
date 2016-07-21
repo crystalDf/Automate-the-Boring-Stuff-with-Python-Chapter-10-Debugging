90cd3d5da5818ce821920f0f7bda9a73392d716e
crystalDf <chendong333@gmail.com>
2016-07-21 21:39:45 +0800

    Chapter 10 Debugging

---
+++ assertion_test.py
@@ -0,0 +1,6 @@
+pod_bay_door_status = 'open'
+assert pod_bay_door_status == 'open', 'The pod bay doors need to be "open".'
+
+pod_bay_door_status = 'I\'m sorry, Dave. I\'m afraid I can\'t do that.'
+
+assert pod_bay_door_status == 'open', 'The pod bay doors need to be "open".'
