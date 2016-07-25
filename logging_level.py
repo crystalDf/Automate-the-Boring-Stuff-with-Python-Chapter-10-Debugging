7836a4e4641664b0877a6830fcd10b068be7b859
crystalDf <chendong333@gmail.com>
2016-07-25 22:12:51 +0800

    Chapter 10 Debugging

--- logging_level.py
+++ logging_level.py
@@ -1,6 +1,6 @@
 import logging
 
-logging.basicConfig(level=logging.ERROR, format=' %(asctime)s - '
+logging.basicConfig(filename='my_program_log.txt', level=logging.INFO, format=' %(asctime)s - '
                                                 '%(levelname)s - %(message)s')
 
 logging.debug('Some debugging details.')
@@ -8,3 +8,7 @@ logging.info('The logging module is working.')
 logging.warning('An error message is about to logged.')
 logging.error('An error has occurred.')
 logging.critical('The program is unable to recover!')
+
+logging.disable(logging.ERROR)
+logging.critical('Critical error! Critical error!')
+logging.error('Error! Error!')
